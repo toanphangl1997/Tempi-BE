a6bcff65721b33ff8478d878bf8b9ca821a82581
toanphan <toanphangl1997@gmail.com>
2025-03-07 00:28:24 +0700

    1

--- src/modules/phone/dto/phone.dto.ts
+++ src/modules/phone/dto/phone.dto.ts
@@ -6,6 +6,7 @@ export class CreatePhoneDto {
   // @IsNotEmpty({ message: 'ID không được để trống' })
   // @IsNumber({}, { message: 'ID phải là số' })
   // id: number;
+  // id: number;
 
   @ApiProperty({ description: 'Tên brand' })
   @IsNotEmpty({ message: 'brand không được để trống' })
